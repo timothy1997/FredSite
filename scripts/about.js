5628ce16a71306b01a43b14faa76efefd6be64a1
Timothy Sherry <timsherry97@gmail.com>
2018-10-01 17:31:19 -0400

    Worked on about page a little

---
+++ scripts/about.js
@@ -0,0 +1,7 @@
+let imageHeading = document.querySelector("h3");
+let imageMessage = document.querySelector("p");
+
+console.log(imageHeading);
+
+$(imageHeading).fadeToggle(4000);
+$(imageMessage).fadeToggle(6000);
